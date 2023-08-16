b62272198c821451ef289d83d8ef285468336173
Morgan Mason <morganmason0606@gmail.com>
2023-08-15 17:24:07 -0700

    added model and database

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'restaurant',
+    #'LittleLemon',
 ]
 
 MIDDLEWARE = [
@@ -76,10 +77,19 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'LittleLemon',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+        'USER':'admindjango',
+        'PASSWORD':'employee@123!',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
+
     }
 }
+
 
 
 # Password validation
